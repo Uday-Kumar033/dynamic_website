2807be2154bec2adb34d75e153126b9abf245e95
Uday Kumar <udaysinghpagurkothi@gmail.com>
2025-08-01 12:43:02 +0530

    Update index.js

--- user-form/index.js
+++ user-form/index.js
@@ -27,14 +27,7 @@ app.listen(PORT, () => {
 });
 
 
-require('dotenv').config();
-const AWS = require('aws-sdk');
 
-AWS.config.update({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
 
 
 const ddb = new AWS.DynamoDB.DocumentClient();
